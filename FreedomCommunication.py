14e744356c4dd3ec222351ca779a5c43ff3bd878
Maurin Thalmann <madotha@hotmail.com>
2018-04-12 13:57:57 +0200

    changes

--- FreedomCommunication.py
+++ FreedomCommunication.py
@@ -8,8 +8,22 @@ def __init__(self):
 
 def start(self):
     print("Starte Gondula #1...")
-    self.conn.write(b'ST')
+    self.conn.write(b'S')
 
 def stop(self):
     print("Stoppe Gondula #1...")
-    self.conn.write(b'SP')
+    self.conn.write(b'H')
+
+def object_detected(self):
+    print("Zielobjekt wurde erkannt...")
+    self.conn.write(b'D')
+
+def device_slower(self):
+    print("Fahrzeug wird langsamer...")
+    self.conn.write(b'-')
+
+def device_faster(self):
+    print("Fahrzeug wird schneller...")
+    self.conn.write(b'+')
+
+
